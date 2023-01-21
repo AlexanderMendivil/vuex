a18eb260ebe5ecbb3a1394f935db233c2d7608e7
Alexander Mendivil <alexanderima@outlook.es>
2023-01-20 20:02:02 -0700

    uso state

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <counter-component/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { defineAsyncComponent } from 'vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterComponent: defineAsyncComponent(() => import('@/components/CounterComponent.vue'))
   }
 }
 </script>
---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Counter - vuex</h1>
+  <h2>Direct access:{{ $store.state.count }}</h2>
+  <h2>computed:{{ countComputed }}</h2>
+  <h2>computed:{{ count }}</h2>
+  <h2>LastMutation:{{ lastMutation }}</h2>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+computed: {
+  countComputed(){
+    return this.$store.state.count
+  },
+  ...mapState({
+    count: state => state.count,
+    lastMutation: state => state.lastMutation
+  })
+}
+}
+</script>
+
+<style>
+
+</style>
